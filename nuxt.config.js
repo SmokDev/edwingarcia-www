73fb7b9d29e828d6bda235548fd0a665ba0c8857
EdwinLeoZm <40440665+EdwinLeoZm@users.noreply.github.com>
2019-07-10 23:02:39 -0500

    setup UIkit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  css: [
+    "~assets/master.sass"
+  ],
+  plugins: [
+    { src:"~plugins/uikit.js", ssr: false}
+  ],
   srcDir: __dirname + "/app",
   build: {
     extend(config, { isClient, loaders: { imgUrl } }){
